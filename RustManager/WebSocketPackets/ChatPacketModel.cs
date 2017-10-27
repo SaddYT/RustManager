ff0f51ddd403d81810fc77c821746c91f79d1095
PsychoTea <bensparkes8@gmail.com>
2017-10-27 15:03:35 +0100

    WebSocket packet models

---
+++ RustManager/WebSocketPackets/ChatPacketModel.cs
@@ -0,0 +1,20 @@
+﻿namespace RustManager.WebSocketPackets
+{
+    class ChatPacketModel : PacketModel
+    {
+        public string ChatMessage;
+        public ulong UserID;
+        public string Username;
+        public string Color;
+        public int Time;
+
+        public void Create(ChatMessageModel model)
+        {
+            this.ChatMessage = model.ChatMessage;
+            this.UserID = model.UserID;
+            this.Username = model.Username;
+            this.Color = model.Color;
+            this.Time = model.Time;
+        }
+    }
+}
