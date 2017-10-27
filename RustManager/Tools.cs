a228a797f4365a5c86fb3f7cd84f3b61f17442bc
PsychoTea <bensparkes8@gmail.com>
2017-10-27 14:10:20 +0100

    [Tools] Added

---
+++ RustManager/Tools.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace RustManager
+{
+    public static class Tools
+    {
+        public static TabPage FindTabPage(TabControl control, string name)
+        {
+            var index = control.TabPages.IndexOfKey(name);
+            return (index != -1) ? control.TabPages[index] : null;
+        }
+    }
+}
